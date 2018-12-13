9c1e9f11592aa01916eb0ccbbabc4e2c7ff382b9
Yashar Asheri <yasheri@MacBook-Pro.eastlink.ca>
2018-12-13 14:00:30 -0400

    added webpack config file, updated home page

--- html_admin/webpack.config.js
+++ html_admin/webpack.config.js
@@ -1,22 +1,41 @@
 const path = require("path");
+const glob = require("glob");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+  // entry: glob.sync(__dirname + "/source/**/[^_]*.slim"),
+  entry: "./source/_partials/_content_box_1.slim",
+  // output: { path: __dirname + "/dist/test" },
+
   module: {
     rules: [
       {
         test: /\.slim$/,
-        exclude: [/node_modules/, /bower_components/],
-        use: {
-          loader: "slim-lang-loader",
-          options: {
-            slimOptions: {
-              pretty: true,
-              chdir: true,
-              include: true
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: "html-loader",
+              options: {
+                minimize: false
+              }
+            },
+            // {
+            //   loader: "file-loader"
+            // },
+            {
+              loader: "slim-lang-loader",
+              options: {
+                slimOptions: { pretty: true, chdir: true, include: true }
+              }
             }
-          }
-        }
+          ]
+        })
       }
     ]
-  }
+  },
+  plugins: [new ExtractTextPlugin("[name].html")],
+  
+  // resolve: {
+  //   extensions: [".slim"]
+  // }
 };
